c4292fd679090144b0b7cbd132c07bf0f77a3544
Lucas Livrone <lucaslivrone@gmail.com>
2022-09-15 19:18:36 -0300

    Desafio 7: update service initiation

--- Scripting_automations/install_nodejs/start_nodejs.sh
+++ Scripting_automations/install_nodejs/start_nodejs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #Initiate the service in one port:
-PORT=3000
-sudo systemctl enable devops@$PORT
-sudo systemctl start devops@$PORT
+#PORT=3000
+#sudo systemctl enable devops@$PORT
+#sudo systemctl start devops@$PORT
 
 
 #Command used to replace string '%i' with the variable $PORT inside the configuration file. (Not needed in this scenario)
@@ -11,4 +11,4 @@ sudo systemctl start devops@$PORT
 
 
 # Initiate service in multiple ports:
-# for port in $(seq 3000 3003); do sudo systemctl enable devops@$port; done
+for port in $(seq 3000 3003); do sudo systemctl enable devops@$port; done
