7519ad59121be559dcc4e2368d1faab0e5144590
Lucas Livrone <lucaslivrone@gmail.com>
2022-12-04 02:15:20 -0300

    poke-challenge update

--- Containers/Desafios/poke-challenge/poke-challenge.sh
+++ Containers/Desafios/poke-challenge/poke-challenge.sh
@@ -14,44 +14,85 @@ chmod +x /usr/local/bin/docker-compose
 git clone https://github.com/roxsross/devops-practice-tools.git
 
 
+# I want to remove the "solutions" files provided by the teacher
+rm -rf devops-practice-tools/docker/challenge/poke-app/user-data/
+rm -rf devops-practice-tools/docker/challenge/poke-app/docker-compose.local.yml
+rm -rf devops-practice-tools/docker/challenge/poke-app/docker-compose.prod.yml
+rm -rf devops-practice-tools/docker/challenge/poke-app/backend-pokemon-app/Dockerfile
+rm -rf devops-practice-tools/docker/challenge/poke-app/frontend-pokemon-app/Dockerfile
+
+
+# Force the code to run on localhost
+cat > devops-practice-tools/docker/challenge/poke-app/frontend-pokemon-app/.env << EOF
+REACT_APP_URL_DEVELOPMENT=http://localhost:8000
+REACT_APP_URL_PRODUCTION=http://localhost:8000
+EOF
+# In the future I would want to use REACT_APP_URL_PRODUCTION=https://api.PUBLIC_IP.nip.io
+# If im running on AWS EC2: PUBLIC_IP=$(curl http://checkip.amazonaws.com)
+
 # Create Dockerfile for the backend
 cd devops-practice-tools/docker/challenge/poke-app/backend-pokemon-app
 cat > Dockerfile << EOF
-FROM python:3.8
+FROM python:3.8-alpine
 WORKDIR /backend
-COPY . /backend
-RUN pip install -r /backend/requirements.txt
+COPY . ./
+RUN pip install -r ./requirements.txt
 EXPOSE 8000
 CMD ["python", "main.py"]
 EOF
 
 
-# Build backend container
-docker build -t backend_pokemon .
-
-
-# Run backend container
-docker run -d --name backend_pokemon -p 8000:8000 backend_pokemon
+# Build and run backend container
+# docker build -t backend_pokemon .
+# docker run -d --name backend_pokemon -p 8000:8000 backend_pokemon
 
 
 # Create Dockerfile for the frontend
 cd ../frontend-pokemon-app/
 cat > Dockerfile << EOF
-FROM node:16
+FROM node:16.3.0-alpine
 WORKDIR /frontend
-ENV PATH /frontend/node_modules/.bin:$PATH
-COPY . /frontend
-RUN npm install --silent
-RUN npm install react-scripts@3.4.1 -g --silent
+COPY package.json ./
+RUN npm install
+COPY . ./
 EXPOSE 3000
 CMD [ "npm", "start" ]
 EOF
 
 
-# Build bfrontendackend container
-docker build -t frontend_pokemon .
+# Build and run frontend container
+# docker build -t frontend_pokemon .
+# docker run -d --name frontend_pokemon -p 3000:3000 frontend_pokemon
 
 
-# Run frontend container
-docker run -d --name frontend_pokemon -p 3000:3000 frontend_pokemon
+# Create docker-compose.yml
+cd ..
+cat > docker-compose.yml << EOF
+version: '3'
+services:
+  backend_pokemon:
+    container_name: backend_pokemon
+    build: ./backend-pokemon-app/
+    ports:
+    - "8000:8000"
+    networks:
+    - poke_network
 
+  frontend_pokemon:
+    container_name: frontend_pokemon
+    build: ./frontend-pokemon-app/
+    ports:
+    - "3000:3000"
+    depends_on:
+    - backend_pokemon
+    networks:
+    - poke_network
+
+networks:
+  poke_network:
+    driver: bridge
+EOF
+
+
+# Build and run docker-compose to run both backend and frontend
+docker-compose up
--- Containers/Desafios/poke-challenge/readme.md
+++ Containers/Desafios/poke-challenge/readme.md
@@ -5,3 +5,7 @@ https://github.com/roxsross/devops-practice-tools/tree/master/docker/challenge/p
 
 ---
 
+
+```
+aws ec2 run-instances --image-id ami-08c40ec9ead489470 --instance-type t3.medium --key-name poke_key --security-group-ids sg-0d7f352c975bd7835 --count 1 --user-data file://poke-challenge.sh --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=poke-challenge-ec2}]'
+```
